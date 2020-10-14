257f4f9a468ed8054802a9777a9de026fecfe93c
Kamil Soczewica <kamil_s11@poczta.fm>
2020-10-14 20:38:55 +0200

    NewFunctionAdded

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
     const addNewTaskButton = document.querySelector(".section__button--submit");
 
-    const addedTask = document.querySelector(".listOfTasks")
 
 
 
@@ -18,35 +17,71 @@
     ];
 
     const render = () => {
-
+        const addedTask = document.querySelector(".listOfTasks")
         let htmlString = "";
         for (const task of tasks) {
 
             htmlString += `
-        
-        <li ${task.done ? "style= \"text-decoration: line-through\"" : ""}>
-
-            <button class="toggleDoneTaskButton"> O</button>
+         
+        <li ${task.done ? "class= \"doneTask\"" : ""}>
+      
+            <button class="toggleDoneTaskButton"> ${task.done ? "✔" : ""}</button>
             ${task.content}
-            <button class="removeTaskButton"> U</button>
-
+            <button class="removeTaskButton">🗑️</button>
+         
         </li>
-
+            
         `;
         }
         addedTask.innerHTML = htmlString;
+
+        removeTaskButton();
+
+        toggleDoneTaskButton();
+
+
+    }
+
+
+    removeTaskButton = () => {
+        const removeButtons = document.querySelectorAll('.removeTaskButton');
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener('click', () => {
+                tasks.splice(index, 1);
+                render();
+            })
+
+        })
+    }
+
+    toggleDoneTaskButton = () => {
+
+        const toggleButtons = document.querySelectorAll('.toggleDoneTaskButton');
+        toggleButtons.forEach((toggleButton, index) => {
+            toggleButton.addEventListener('click', () => {
+
+
+                tasks[index].done = !tasks[index].done;
+
+                render();
+            })
+
+        })
+
+
+
 
     }
 
     const addTask = (event) => {
 
         event.preventDefault();
-
-
         const taskToDo = document.querySelector(".section__form__input--newTask");
         taskToDo.focus();
+
+
         if (taskToDo.value == "") {
-            
+
             return false;
         }
 
@@ -54,7 +89,7 @@
             content: taskToDo.value.trim()
         })
         taskToDo.value = "";
-        
+
         render();
     }
 
